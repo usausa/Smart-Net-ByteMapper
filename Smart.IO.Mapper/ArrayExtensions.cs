9ffaf076fbb571c7b3583affa8b86f6a0f0cf14f
usausa <machi.pon@gmail.com>
2016-05-25 22:25:44 +0900

    Update ArrayExtensions

--- Smart.IO.Mapper/ArrayExtensions.cs
+++ Smart.IO.Mapper/ArrayExtensions.cs
@@ -799,6 +799,188 @@
             return true;
         }
 
-        // TODO
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this byte[] array, int offset, byte[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this short[] array, int offset, short[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this int[] array, int offset, int[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this long[] array, int offset, long[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this float[] array, int offset, float[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this double[] array, int offset, double[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this bool[] array, int offset, bool[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="offset"></param>
+        /// <param name="other"></param>
+        /// <param name="otherOffset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:パブリック メソッドの引数の検証", Justification = "Extensions")]
+        public static bool ArrayEquals(this char[] array, int offset, char[] other, int otherOffset, int length)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                if (array[offset + i].CompareTo(other[otherOffset + i]) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
