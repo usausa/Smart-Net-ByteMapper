f41b0a6e6e0ba08cc9d1f4466f070404a75c5f10
usausa <machi.pon@gmail.com>
2018-03-19 17:42:30 +0900

    Update type extensions

--- Smart.IO.Mapper/TypeMapperExtensions.cs
+++ Smart.IO.Mapper/TypeMapperExtensions.cs
@@ -1,5 +1,8 @@
 ﻿namespace Smart.IO.Mapper
 {
+    using System.Collections.Generic;
+    using System.IO;
+
     public static class TypeMapperExtensions
     {
         public static T FromByte<T>(ITypeMapper<T> mapper, byte[] buffer)
@@ -12,12 +15,44 @@
 
             var target = new T();
             mapper.FromByte(buffer, target);
-
             return target;
         }
 
-        // TODO Stream read ?
-        // TODO Writer, bytes & stream ?
-        // TODO T, byte[]を返す拡張メソッド
+        public static IEnumerable<T> FromBytes<T>(ITypeMapper<T> mapper, IEnumerable<byte[]> source)
+            where T : new()
+        {
+            foreach (var buffer in source)
+            {
+                if (buffer.Length < mapper.Size)
+                {
+                    continue;
+                }
+
+                var target = new T();
+                mapper.FromByte(buffer, target);
+                yield return target;
+            }
+        }
+
+        public static IEnumerable<T> FromBytes<T>(ITypeMapper<T> mapper, Stream stream)
+            where T : new()
+        {
+            var buffer = new byte[mapper.Size];
+            while (stream.Read(buffer, 0, buffer.Length) == buffer.Length)
+            {
+                if (buffer.Length < mapper.Size)
+                {
+                    continue;
+                }
+
+                var target = new T();
+                mapper.FromByte(buffer, target);
+                yield return target;
+            }
+        }
+
+        // TODO buffer skip
+        // TODO IE<byte[]>
+        // TODO Stream write
     }
 }
