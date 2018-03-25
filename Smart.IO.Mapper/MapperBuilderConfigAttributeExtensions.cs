409beef3200dc9571d970f6b5603e75c8d72cdf4
usausa <machi.pon@gmail.com>
2018-03-25 17:17:54 +0900

    Add extensions

---
+++ Smart.IO.Mapper/MapperBuilderConfigAttributeExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace Smart.IO.Mapper
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class MapperBuilderConfigAttributeExtensions
+    {
+        public static MapperBuilderConfig Map<T>(this MapperBuilderConfig config)
+        {
+            return config.Map(typeof(T));
+        }
+
+        public static MapperBuilderConfig Map(this MapperBuilderConfig config, Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            // TODO check attribute
+
+            // TODO common
+
+            return config;
+        }
+
+        public static MapperBuilderConfig Map(this MapperBuilderConfig config, IEnumerable<Type> types)
+        {
+            if (types == null)
+            {
+                throw new ArgumentNullException(nameof(types));
+            }
+
+            // TODO common where attribute
+
+            return config;
+        }
+    }
+}
---
+++ Smart.IO.Mapper/MapperBuilderConfigExpressionExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Smart.IO.Mapper
+{
+    public static class MapperBuilderConfigExpressionExtensions
+    {
+    }
+}
