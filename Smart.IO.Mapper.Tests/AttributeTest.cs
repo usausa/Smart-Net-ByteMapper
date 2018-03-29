94fd7aacc55d07e7f4440090ded61a3f80349619
usausa <machi.pon@gmail.com>
2018-03-29 17:38:20 +0900

    Update test

--- Smart.IO.Mapper.Tests/AttributeTest.cs
+++ Smart.IO.Mapper.Tests/AttributeTest.cs
@@ -18,10 +18,11 @@
             var mapper = byteMapper.Create<Data>();
 
             var data = new Data();
-            mapper.FromByte(Encoding.ASCII.GetBytes("  1234abc "), data);
+            mapper.FromByte(Encoding.ASCII.GetBytes("  1234abc 1"), data);
 
             Assert.Equal(1234, data.IntValue);
             Assert.Equal("abc", data.StringValue);
+            Assert.True(data.BoolValue);
         }
 
         [Map(Size)]
@@ -29,11 +30,13 @@
         {
             private const int IntValueLength = 6;
             private const int StringValueLength = 4;
+            private const int BoolValueLength = 1;
             private const int DelimitterLength = 2;
 
             private const int IntValueOffset = 0;
             private const int StringValueOffset = IntValueOffset + IntValueLength;
-            private const int DelimitterOffset = StringValueOffset + StringValueLength;
+            private const int BoolValueOffset = StringValueOffset + StringValueLength;
+            private const int DelimitterOffset = BoolValueOffset + BoolValueLength;
 
             private const int Size = DelimitterOffset + DelimitterLength;
 
@@ -42,6 +45,9 @@
 
             [String(StringValueOffset, Length = StringValueLength)]
             public string StringValue { get; set; }
+
+            [BoolBinary(BoolValueOffset)]
+            public bool BoolValue { get; set; }
         }
     }
 }
