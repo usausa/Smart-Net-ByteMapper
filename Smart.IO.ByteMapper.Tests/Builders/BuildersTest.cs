c7794220a8945f4e02adf2b14d0fea58c80ff812
usausa <machi.pon@gmail.com>
2018-04-10 17:24:33 +0900

    Update test

---
+++ Smart.IO.ByteMapper.Tests/Builders/BuildersTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Smart.IO.ByteMapper.Builders
+{
+    using Smart.IO.ByteMapper.Mock;
+
+    using Xunit;
+
+    public class BuildersTest
+    {
+        [Fact]
+        public void CoverageFix()
+        {
+            Assert.Equal(0, new BinaryConverterBuilder().CalcSize(typeof(object)));
+            Assert.Null(new BinaryConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+
+            Assert.Null(new BooleanConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+
+            Assert.Null(new ByteConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+
+            Assert.Null(new BytesConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+
+            Assert.Null(new DateTimeTextConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+
+            Assert.Null(new NumberTextConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+
+            Assert.Null(new TextConverterBuilder().CreateConverter(new MockBuilderContext(), typeof(object)));
+        }
+    }
+}
---
+++ Smart.IO.ByteMapper.Tests/Mock/MockBuilderContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Smart.IO.ByteMapper.Mock
+{
+    using Smart.ComponentModel;
+
+    using Smart.IO.ByteMapper.Builders;
+
+    public class MockBuilderContext : IBuilderContext
+    {
+        public IComponentContainer Components { get; } = new ComponentConfig().ToContainer();
+
+        public T GetParameter<T>(string key)
+        {
+            return default;
+        }
+    }
+}
