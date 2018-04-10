ca1837d9caa8288411737342822e691b8399d589
usausa <machi.pon@gmail.com>
2018-04-10 13:26:25 +0900

    Remove work files

--- Smart.IO.Mapper.Benchmark/ConverterBenchmark.cs
+++ Smart.IO.Mapper.Benchmark/ConverterBenchmark.cs
@@ -1,75 +1,75 @@
-﻿namespace Smart.IO.Mapper.Benchmark
-{
-    using System.Globalization;
-    using System.Text;
+﻿//namespace Smart.IO.Mapper.Benchmark
+//{
+//    using System.Globalization;
+//    using System.Text;
 
-    using BenchmarkDotNet.Attributes;
+//    using BenchmarkDotNet.Attributes;
 
-    using Smart.IO.Mapper;
-    using Smart.IO.Mapper.Converters;
+//    using Smart.IO.Mapper;
+//    using Smart.IO.Mapper.Converters;
 
-    [Config(typeof(BenchmarkConfig))]
-    public class ConverterBenchmark
-    {
-        //private BigEndianIntBinaryConverter intBinaryConverter;
+//    [Config(typeof(BenchmarkConfig))]
+//    public class ConverterBenchmark
+//    {
+//        private BigEndianIntBinaryConverter intBinaryConverter;
 
-        //private byte[] intBinaryBuffer;
+//        private byte[] intBinaryBuffer;
 
-        //private TextConverter stringW10Converter;
+//        private TextConverter stringW10Converter;
 
-        //private byte[] stringW10Buffer;
+//        private byte[] stringW10Buffer;
 
-        //private IntTextConverter intText8Converter;
+//        private IntTextConverter intText8Converter;
 
-        //private byte[] intText8Buffer;
+//        private byte[] intText8Buffer;
 
-        //[GlobalSetup]
-        //public void Setup()
-        //{
-        //    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+//        [GlobalSetup]
+//        public void Setup()
+//        {
+//            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-        //    var numberEncoding = Encoding.ASCII;
-        //    var stringEncoding = Encoding.GetEncoding(932);
+//            var numberEncoding = Encoding.ASCII;
+//            var stringEncoding = Encoding.GetEncoding(932);
 
-        //    intBinaryConverter = new BigEndianIntBinaryConverter();
-        //    intBinaryBuffer = new byte[sizeof(int)];
+//            intBinaryConverter = new BigEndianIntBinaryConverter();
+//            intBinaryBuffer = new byte[sizeof(int)];
 
-        //    stringW10Converter = new TextConverter(
-        //        20,
-        //        stringEncoding,
-        //        true,
-        //        Padding.Right,
-        //        0x20);
-        //    stringW10Buffer = stringEncoding.GetBytes("あいうえお          ");
+//            stringW10Converter = new TextConverter(
+//                20,
+//                stringEncoding,
+//                true,
+//                Padding.Right,
+//                0x20);
+//            stringW10Buffer = stringEncoding.GetBytes("あいうえお          ");
 
-        //    intText8Converter = new IntTextConverter(
-        //        8,
-        //        numberEncoding,
-        //        true,
-        //        Padding.Left,
-        //        0x20,
-        //        NumberStyles.Integer,
-        //        NumberFormatInfo.InvariantInfo,
-        //        typeof(int));
-        //    intText8Buffer = numberEncoding.GetBytes("  100000");
-        //}
+//            intText8Converter = new IntTextConverter(
+//                8,
+//                numberEncoding,
+//                true,
+//                Padding.Left,
+//                0x20,
+//                NumberStyles.Integer,
+//                NumberFormatInfo.InvariantInfo,
+//                typeof(int));
+//            intText8Buffer = numberEncoding.GetBytes("  100000");
+//        }
 
-        //[Benchmark]
-        //public void ReadIntBinary()
-        //{
-        //    intBinaryConverter.Read(intBinaryBuffer, 0);
-        //}
+//        [Benchmark]
+//        public void ReadIntBinary()
+//        {
+//            intBinaryConverter.Read(intBinaryBuffer, 0);
+//        }
 
-        //[Benchmark]
-        //public void ReadStringW10()
-        //{
-        //    stringW10Converter.Read(stringW10Buffer, 0);
-        //}
+//        [Benchmark]
+//        public void ReadStringW10()
+//        {
+//            stringW10Converter.Read(stringW10Buffer, 0);
+//        }
 
-        //[Benchmark]
-        //public void ReadIntText8()
-        //{
-        //    intText8Converter.Read(intText8Buffer, 0);
-        //}
-    }
-}
+//        [Benchmark]
+//        public void ReadIntText8()
+//        {
+//            intText8Converter.Read(intText8Buffer, 0);
+//        }
+//    }
+//}
