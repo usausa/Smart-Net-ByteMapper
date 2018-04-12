7cf02951fde81aea4a17cdeb8559543662804cc7
usausa <machi.pon@gmail.com>
2018-04-12 13:10:35 +0900

    Add map key test

---
+++ Smart.IO.ByteMapper.Tests/MapKeyTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Smart.IO.ByteMapper
+{
+    using Xunit;
+
+    public class MapKeyTest
+    {
+        [Fact]
+        public void MapKeyCompare()
+        {
+            var key1 = new MapKey(typeof(object), string.Empty);
+            var key1B = key1;
+
+            // Compare to null
+            Assert.False(key1.Equals(null));
+
+            // Compar to self
+            Assert.True(key1.Equals(key1B));
+
+            // Compar to same
+            Assert.True(key1.Equals(new MapKey(typeof(object), string.Empty)));
+
+            // Compar to different type
+            Assert.False(key1.Equals(new MapKey(typeof(string), string.Empty)));
+
+            // Compar to different name
+            Assert.False(key1.Equals(new MapKey(typeof(object), "x")));
+
+            // Compar to another type
+            Assert.False(key1.Equals(new object()));
+        }
+    }
+}
