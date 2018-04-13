d5d36acf999657adb80d943cf3be5b77056270a7
usausa <machi.pon@gmail.com>
2018-04-13 17:43:02 +0900

    Add web example project

---
+++ Example.WebApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Example.WebApplication
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
