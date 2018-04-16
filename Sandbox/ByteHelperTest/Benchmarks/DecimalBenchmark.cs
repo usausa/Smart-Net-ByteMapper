755cd09e1dbe648d5348efaec4a82abd4ae9a25e
usausa <machi.pon@gmail.com>
2018-04-16 17:24:54 +0900

    Update decimal benchmark

--- Sandbox/ByteHelperTest/Benchmarks/DecimalBenchmark.cs
+++ Sandbox/ByteHelperTest/Benchmarks/DecimalBenchmark.cs
@@ -13,6 +13,10 @@
 
         private static readonly byte[] Bytes28 = Encoding.ASCII.GetBytes("12345678901234567890123456.78");
 
+        //private static readonly decimal Value8 = 123456.78m;
+
+        //private static readonly decimal Value28 = 12345678901234567890123456.78m;
+
         [Benchmark]
         public void ParseDefault8()
         {
@@ -36,5 +40,17 @@
         {
             ByteHelper.TryParseDecimal(Bytes28, 0, Bytes28.Length, out var _);
         }
+
+        //[Benchmark]
+        //public void FormatDefault8()
+        //{
+        //    Encoding.ASCII.GetBytes(Value8.ToString("0000000.000"));
+        //}
+
+        //[Benchmark]
+        //public void FormatDefault28()
+        //{
+        //    Encoding.ASCII.GetBytes(Value28.ToString("000000000000000000000000000.000"));
+        //}
     }
 }
