b31a0fcba2db0a8c9ae36aaa594f38e634011199
usausa <machi.pon@gmail.com>
2018-04-27 15:54:53 +0900

    Update input formatter

--- Smart.IO.ByteMapper.AspNetCore/AspNetCore/Formatters/ByteMapperInputFormatter.cs
+++ Smart.IO.ByteMapper.AspNetCore/AspNetCore/Formatters/ByteMapperInputFormatter.cs
@@ -1,18 +1,16 @@
 ﻿namespace Smart.AspNetCore.Formatters
 {
-    using System;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc.Formatters;
 
     using Smart.AspNetCore.Helpers;
-    using Smart.Collections.Concurrent;
     using Smart.IO.ByteMapper;
     using Smart.Reflection;
 
     public class ByteMapperInputFormatter : InputFormatter
     {
-        private readonly ThreadsafeTypeHashArrayMap<FactoryMetadata> factoryCache = new ThreadsafeTypeHashArrayMap<FactoryMetadata>();
+        //private readonly ThreadsafeTypeHashArrayMap<IGenericReader>
 
         private readonly MapperFactory mapperFactory;
 
@@ -31,22 +29,22 @@
 
         public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
         {
-            var profile = context.HttpContext.Items.TryGetValue(Consts.ProfileKey, out var value) ? value as string : null;
+            //var profile = context.HttpContext.Items.TryGetValue(Consts.ProfileKey, out var value) ? value as string : null;
 
             //var request = context.HttpContext.Request;
             var type = TypeHelper.GetEnumerableElementType(context.ModelType);
             var multiple = type != null;
-            type = type ?? context.ModelType;
+            //type = type ?? context.ModelType;
 
-            var mapper = mapperFactory.Create(type, profile);
+            //var mapper = mapperFactory.Create(type, profile);
             if (multiple)
             {
                 // TODO GenericなGeneratorを作る？
 
                 // TODO IE対応？
-                var factory = factoryCache.AddIfNotExist(type, CreateFactoryMetadata);
+                //var factory = factoryCache.AddIfNotExist(type, CreateFactoryMetadata);
 
-                var len = context.HttpContext.Request.ContentLength;
+                //var len = context.HttpContext.Request.ContentLength;
 
                 //delegateFactory.CreateArrayAllocator()
 
@@ -55,31 +53,9 @@
                 // TODO
                 return InputFormatterResult.SuccessAsync(null);
             }
-            else
-            {
-                // TODO
-                return InputFormatterResult.SuccessAsync(null);
-            }
-        }
 
-        private FactoryMetadata CreateFactoryMetadata(Type type)
-        {
-            return new FactoryMetadata(
-                delegateFactory.CreateArrayAllocator(type),
-                delegateFactory.CreateFactory0(type.GetConstructor(Type.EmptyTypes)));
-        }
-
-        private class FactoryMetadata
-        {
-            public Func<int, Array> ArrayFactory { get; }
-
-            public Func<object> Factory { get; }
-
-            public FactoryMetadata(Func<int, Array> arrayFactory, Func<object> factory)
-            {
-                ArrayFactory = arrayFactory;
-                Factory = factory;
-            }
+            // TODO single
+            return InputFormatterResult.SuccessAsync(null);
         }
     }
 }
