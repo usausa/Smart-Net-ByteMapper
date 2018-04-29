738f1ba306cdb15e4ab03789ba4008da07ae8c6b
usausa <machi.pon@gmail.com>
2018-04-29 13:40:21 +0900

    Fix inspection

--- Example.WebApplication/Controllers/MapController.cs
+++ Example.WebApplication/Controllers/MapController.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace Example.WebApplication.Controllers
+﻿namespace Example.WebApplication.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     using Example.WebApplication.Models;
