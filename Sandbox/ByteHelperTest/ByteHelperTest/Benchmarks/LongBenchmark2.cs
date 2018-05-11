699d3dd6c29cf55ca01eda7780874db0bdb41d09
usausa <machi.pon@gmail.com>
2018-05-11 11:38:51 +0900

    Update sandbox

---
+++ Sandbox/ByteHelperTest/ByteHelperTest/Benchmarks/LongBenchmark2.cs
@@ -0,0 +1,46 @@
+﻿namespace ByteHelperTest.Benchmarks
+{
+    using BenchmarkDotNet.Attributes;
+
+    [Config(typeof(BenchmarkConfig))]
+    public class LongBenchmark2
+    {
+        private static readonly long Value8 = 12345678;
+
+        private static readonly long Value19 = 1234567890123456789;
+
+        [Params(Padding.Left, Padding.Right)]
+        public Padding Padding { get; set; }
+
+        [Params(true, false)]
+        public bool ZeroFill { get; set; }
+
+        [Benchmark]
+        public void FormatOld8()
+        {
+            var buffer = new byte[8];
+            ByteHelper2.FormatInt64(buffer, 0, buffer.Length, Value8, Padding, ZeroFill, 0x30);
+        }
+
+        [Benchmark]
+        public void Format8()
+        {
+            var buffer = new byte[8];
+            ByteHelper2.FormatInt64(buffer, 0, buffer.Length, Value8, Padding, ZeroFill, 0x30);
+        }
+
+        [Benchmark]
+        public void FormatOld19()
+        {
+            var buffer = new byte[19];
+            ByteHelper2.FormatInt64(buffer, 0, buffer.Length, Value19, Padding, ZeroFill, 0x30);
+        }
+
+        [Benchmark]
+        public void Format19()
+        {
+            var buffer = new byte[19];
+            ByteHelper2.FormatInt64(buffer, 0, buffer.Length, Value19, Padding, ZeroFill, 0x30);
+        }
+    }
+}
--- Sandbox/ByteHelperTest/ByteHelperTest/ByteHelper2.cs
+++ Sandbox/ByteHelperTest/ByteHelperTest/ByteHelper2.cs
@@ -5,8 +5,197 @@
 
     public static class ByteHelper2
     {
-        // TODO to IntegerTable copy speed up ? 長さもわかるように？
-        private const byte Zero = (byte)'0';
+        private const byte Minus = (byte)'-';
+        private const byte Num0 = (byte)'0';
+        private const byte Dot = (byte)'.';
+        private const byte Comma = (byte)',';
+        private const int DotDiff = Dot - Num0;
+        private const int CommaDiff = Comma - Num0;
+
+        private const int Int32MinValueDiv10 = Int32.MinValue / 10;
+        private const byte Int32MinValueMod10 = Num0 + -(Int32.MinValue % 10);
+        private const long Int64MinValueDiv10 = Int64.MinValue / 10;
+        private const byte Int64MinValueMod10 = (byte)(Num0 + -(Int64.MinValue % 10));
+
+        private const int NegativeBitFlag = unchecked((int)0x80000000);
+
+        //--------------------------------------------------------------------------------
+        // Decimal
+        //--------------------------------------------------------------------------------
+
+        public static unsafe void FormatInt64Old(byte[] bytes, int index, int length, long value, Padding padding, bool zerofill, byte filler)
+        {
+            fixed (byte* pBytes = &bytes[index])
+            {
+                if ((padding == Padding.Left) || zerofill)
+                {
+                    var i = length - 1;
+
+                    if ((value == Int64.MinValue) && (i >= 0))
+                    {
+                        *(pBytes + i--) = Int64MinValueMod10;
+                        value = Int64MinValueDiv10;
+                    }
+
+                    var negative = value < 0;
+                    if (negative)
+                    {
+                        value = -value;
+                    }
+
+                    while (i >= 0)
+                    {
+                        *(pBytes + i--) = (byte)(Num0 + (value % 10));
+
+                        value /= 10;
+                        if (value == 0)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (zerofill)
+                    {
+                        while (i >= (negative ? 1 : 0))
+                        {
+                            *(pBytes + i--) = Num0;
+                        }
+
+                        if (negative && (i >= 0))
+                        {
+                            *pBytes = Minus;
+                        }
+                    }
+                    else
+                    {
+                        if (negative && (i >= 0))
+                        {
+                            *(pBytes + i--) = Minus;
+                        }
+
+                        while (i >= 0)
+                        {
+                            *(pBytes + i--) = filler;
+                        }
+                    }
+                }
+                else
+                {
+                    var i = 0;
+
+                    if ((value == Int64.MinValue) && (i < length))
+                    {
+                        *(pBytes + i++) = Int64MinValueMod10;
+                        value = Int64MinValueDiv10;
+                    }
+
+                    var negative = value < 0;
+                    if (negative)
+                    {
+                        value = -value;
+                    }
+
+                    while (i < length)
+                    {
+                        *(pBytes + i++) = (byte)(Num0 + (value % 10));
+
+                        value /= 10;
+                        if (value == 0)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (negative && (i < length))
+                    {
+                        *(pBytes + i++) = Minus;
+                    }
+
+                    ReverseBytes(pBytes, i);
+
+                    while (i < length)
+                    {
+                        *(pBytes + i++) = filler;
+                    }
+                }
+            }
+        }
+
+        public static unsafe void FormatInt64(byte[] bytes, int index, int length, long value, Padding padding, bool zerofill, byte filler)
+        {
+            fixed (byte* pBytes = &bytes[index])
+            {
+                var i = 0;
+
+                if ((value == Int64.MinValue) && (i < length))
+                {
+                    *(pBytes + i++) = Int64MinValueMod10;
+                    value = Int64MinValueDiv10;
+                }
+
+                var negative = value < 0;
+                if (negative)
+                {
+                    value = -value;
+                }
+
+                // Work 20
+                var work = stackalloc byte[20];
+                var writed = DigitTable.GetLongBuffer20(work, value);
+
+                var copy = writed > length - i ? length - i : writed;
+                Buffer.MemoryCopy(work, pBytes + i, copy, copy);
+                i += copy;
+
+                if (zerofill)
+                {
+                    var max = negative ? length - 1 : length;
+                    while (i < max)
+                    {
+                        *(pBytes + i++) = Num0;
+                    }
+
+                    if (negative && (i < length))
+                    {
+                        *(pBytes + i) = Minus;
+                    }
+
+                    ReverseBytes(pBytes, length);
+                }
+                else if (padding == Padding.Left)
+                {
+                    if (negative && (i < length))
+                    {
+                        *(pBytes + i++) = Minus;
+                    }
+
+                    while (i < length)
+                    {
+                        *(pBytes + i++) = filler;
+                    }
+
+                    ReverseBytes(pBytes, length);
+                }
+                else
+                {
+                    if (negative && (i < length))
+                    {
+                        *(pBytes + i++) = Minus;
+                    }
+
+                    ReverseBytes(pBytes, i);
+
+                    while (i < length)
+                    {
+                        *(pBytes + i++) = filler;
+                    }
+                }
+            }
+        }
+
+        //--------------------------------------------------------------------------------
+        // Decimal
+        //--------------------------------------------------------------------------------
 
         public static unsafe void FormatDecimal0(
             byte[] bytes,
@@ -31,19 +220,21 @@
             DecimalTable.AddBitBlockValue(ref lo, ref hi, 10, (bits[2] >> 16) & 0b11111111);
             DecimalTable.AddBitBlockValue(ref lo, ref hi, 11, (bits[2] >> 24) & 0b11111111);
 
-            var work = stackalloc byte[Math.Max(length, 32)];
-            IntegerTable.GetLongBuffer16(work, lo);
-            IntegerTable.GetLongBuffer16(work + 16, hi);
+            var work = stackalloc byte[32];
+            var writed = DigitTable.GetLongBuffer16(work, lo);
+            writed += DigitTable.GetLongBuffer16(work + 16, hi);
 
-            // TODO tune, reverse
-            fixed (byte* pBuffer = &bytes[offset])
+            fixed (byte* pBytes = &bytes[offset])
             {
-                for (var i = 0; i < length; i++)
+                var copy = writed > length ? length : writed;
+                Buffer.MemoryCopy(work, pBytes, copy, copy);
+
+                for (var i = writed; i < length; i++)
                 {
-                    pBuffer[offset + i] = (byte)(work[i] + Zero);
+                    pBytes[offset + i] = 0x30;
                 }
 
-                ReverseBytes(pBuffer, length);
+                ReverseBytes(pBytes, length);
             }
         }
 
