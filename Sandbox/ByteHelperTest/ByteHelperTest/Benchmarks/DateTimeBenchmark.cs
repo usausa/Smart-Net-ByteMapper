97e9f61ed1c7277453c01b428d9689ecd6d45fca
usausa <machi.pon@gmail.com>
2018-05-18 14:51:08 +0900

    Update sandbox

--- Sandbox/ByteHelperTest/ByteHelperTest/Benchmarks/DateTimeBenchmark.cs
+++ Sandbox/ByteHelperTest/ByteHelperTest/Benchmarks/DateTimeBenchmark.cs
@@ -1,7 +1,6 @@
 ﻿namespace ByteHelperTest.Benchmarks
 {
     using System;
-    using System.Globalization;
     using System.Text;
 
     using BenchmarkDotNet.Attributes;
@@ -12,6 +11,51 @@
         private static readonly byte[] Bytes17 = Encoding.ASCII.GetBytes("20001231235959");
 
         private static readonly DateTime Date = new DateTime(2199, 12, 31, 23, 59, 59, 123);
+
+        //private ByteHelper4.IDateTimeBlockConverter[] converters8A;
+
+        //private ByteHelper4.IDateTimeBlockConverter[] converters17A;
+
+        private ByteHelper4.DateTimeEntry[] entries8B;
+
+        private ByteHelper4.DateTimeEntry[] entries17B;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            //converters8A = new ByteHelper4.IDateTimeBlockConverter[]
+            //{
+            //    new ByteHelper4.Year4BlockConverter(0),
+            //    new ByteHelper4.Month2BlockConverter(4),
+            //    new ByteHelper4.Day2BlockConverter(6)
+            //};
+            //converters17A = new ByteHelper4.IDateTimeBlockConverter[]
+            //{
+            //    new ByteHelper4.Year4BlockConverter(0),
+            //    new ByteHelper4.Month2BlockConverter(4),
+            //    new ByteHelper4.Day2BlockConverter(6),
+            //    new ByteHelper4.Hour2BlockConverter(8),
+            //    new ByteHelper4.Minute2BlockConverter(10),
+            //    new ByteHelper4.Second2BlockConverter(12),
+            //    new ByteHelper4.Millisecond3BlockConverter(14),
+            //};
+            entries8B = new[]
+            {
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Year, 4, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Month, 2, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Day, 2, null)
+            };
+            entries17B = new[]
+            {
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Year, 4, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Month, 2, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Day, 2, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Hour, 2, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Minute, 2, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Second, 2, null),
+                new ByteHelper4.DateTimeEntry(ByteHelper4.DateTimePart.Millisecond, 2, null)
+            };
+        }
 
         // Parse
 
@@ -38,15 +82,75 @@
         [Benchmark]
         public void FormatCustom8()
         {
-            var buffer = new byte[8];
-            ByteHelper4.FormatDateTime(buffer, 0, "yyyyMMdd", Date);
+            ByteHelper4.FormatDateTime(Bytes17, 0, "yyyyMMdd", Date);
         }
+
+        [Benchmark]
+        public void FormatCustom8B()
+        {
+            ByteHelper4.FormatDateTime2(Bytes17, 0, entries8B, Date);
+        }
+
+        // MEMO slow
+        //[Benchmark]
+        //public unsafe void FormatCustom8A()
+        //{
+        //    ByteHelper4.GetDatePart(Date.Ticks, out var year, out var month, out var day);
+
+        //    var context = new ByteHelper4.DateTimeContext
+        //    {
+        //        Year = year,
+        //        Month = month,
+        //        Day = day,
+        //        Ticks = Date.Ticks
+        //    };
+
+        //    fixed (byte* pBytes = &Bytes17[0])
+        //    {
+        //        for (var i = 0; i < converters8A.Length; i++)
+        //        {
+        //            converters8A[i].Write(pBytes, ref context);
+        //        }
+        //    }
+
+        //    ByteHelper4.FormatDateTime(Bytes17, 0, "yyyyMMdd", Date);
+        //}
 
         [Benchmark]
         public void FormatCustom17()
         {
-            var buffer = new byte[17];
-            ByteHelper4.FormatDateTime(buffer, 0, "yyyyMMddHHmmssfff", Date);
+            ByteHelper4.FormatDateTime(Bytes17, 0, "yyyyMMddHHmmssfff", Date);
         }
+
+        [Benchmark]
+        public void FormatCustom17B()
+        {
+            ByteHelper4.FormatDateTime2(Bytes17, 0, entries17B, Date);
+        }
+
+        // MEMO slow
+        //[Benchmark]
+        //public unsafe void FormatCustom17A()
+        //{
+        //    ByteHelper4.GetDatePart(Date.Ticks, out var year, out var month, out var day);
+
+        //    var context = new ByteHelper4.DateTimeContext
+        //    {
+        //        Year = year,
+        //        Month = month,
+        //        Day = day,
+        //        Ticks = Date.Ticks
+        //    };
+
+        //    fixed (byte* pBytes = &Bytes17[0])
+        //    {
+        //        for (var i = 0; i < converters17A.Length; i++)
+        //        {
+        //            converters17A[i].Write(pBytes, ref context);
+        //        }
+        //    }
+
+        //    ByteHelper4.FormatDateTime(Bytes17, 0, "yyyyMMdd", Date);
+        //}
     }
 }
--- Sandbox/ByteHelperTest/ByteHelperTest/ByteHelper4.cs
+++ Sandbox/ByteHelperTest/ByteHelperTest/ByteHelper4.cs
@@ -321,5 +321,366 @@
                 *(pBytes + i++) = (byte)(Num0 + value);
             }
         }
+
+        //--------------------------------------------------------------------------------
+        // DateTime3
+        //--------------------------------------------------------------------------------
+
+        public enum DateTimePart
+        {
+            Year, Month, Day, Hour, Minute, Second, Millisecond, Other
+        }
+
+        public sealed class DateTimeEntry
+        {
+            public DateTimePart Part { get; }
+
+            public int Length { get; }
+
+            public byte[] Bytes { get; }
+
+            public DateTimeEntry(DateTimePart part, int length, byte[] bytes)
+            {
+                Part = part;
+                Length = length;
+                Bytes = bytes;
+            }
+        }
+
+        public static unsafe void FormatDateTime2(byte[] bytes, int index, DateTimeEntry[] entries, DateTime dateTime)
+        {
+            fixed (byte* pBytes = &bytes[index])
+            {
+                var offset = 0;
+                for (var i = 0; i < entries.Length; i++)
+                {
+                    var entry = entries[i];
+                    var part = entry.Part;
+                    var length = entry.Length;
+                    var pBase = pBytes + offset;
+
+                    if (part == DateTimePart.Year)
+                    {
+                        FormatDateTimePart(pBase, dateTime.Year, length);
+                    }
+                    else if (part == DateTimePart.Month)
+                    {
+                        FormatDateTimePart(pBase, dateTime.Month, length);
+                    }
+                    else if (part == DateTimePart.Day)
+                    {
+                        FormatDateTimePart(pBase, dateTime.Day, length);
+                    }
+                    else if (part == DateTimePart.Hour)
+                    {
+                        FormatDateTimePart(pBase, dateTime.Hour, length);
+                    }
+                    else if (part == DateTimePart.Minute)
+                    {
+                        FormatDateTimePart(pBase, dateTime.Minute, length);
+                    }
+                    else if (part == DateTimePart.Second)
+                    {
+                        FormatDateTimePart(pBase, dateTime.Second, length);
+                    }
+                    else if (part == DateTimePart.Millisecond)
+                    {
+                        FormatDateTimeMillisecond(pBase, dateTime.Millisecond, length);
+                    }
+                    else
+                    {
+                        var source = entry.Bytes;
+                        for (var j = 0; j < source.Length; j++)
+                        {
+                            *(pBase + j) = source[j];
+                        }
+                    }
+
+                    offset += length;
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void FormatDateTimePart(byte* pBytes, int value, int length)
+        {
+            var offset = length - 1;
+            for (var j = 0; j < length - 1; j++)
+            {
+                OperationHelper.DivMod10Signed(value, out var div, out var mod);
+                *(pBytes + offset--) = (byte)(Num0 + mod);
+                value = div;
+            }
+
+            *(pBytes + offset) = (byte)(Num0 + value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void FormatDateTimeMillisecond(byte* pBytes, int value, int length)
+        {
+            *pBytes = (byte)(Num0 + (value / 100));
+            value = value % 100;
+
+            if (length > 1)
+            {
+                OperationHelper.DivMod10Signed(value, out var div, out var mod);
+                *(pBytes + 1) = (byte)(Num0 + div);
+                value = mod;
+            }
+
+            if (length > 2)
+            {
+                *(pBytes + 2) = (byte)(Num0 + value);
+            }
+        }
+
+        //--------------------------------------------------------------------------------
+        // DateTime2(slow)
+        //--------------------------------------------------------------------------------
+
+        //// Number of days in a non-leap year
+        //private const int DaysPerYear = 365;
+        //// Number of days in 4 years
+        //private const int DaysPer4Years = DaysPerYear * 4 + 1;       // 1461
+        //// Number of days in 100 years
+        //private const int DaysPer100Years = DaysPer4Years * 25 - 1;  // 36524
+        //// Number of days in 400 years
+        //private const int DaysPer400Years = DaysPer100Years * 4 + 1; // 146097
+
+        //public static void GetDatePart(long ticks, out int year, out int month, out int day)
+        //{
+        //    // n = number of days since 1/1/0001
+        //    int n = (int)(ticks / TicksPerDay);
+        //    // y400 = number of whole 400-year periods since 1/1/0001
+        //    int y400 = n / DaysPer400Years;
+        //    // n = day number within 400-year period
+        //    n -= y400 * DaysPer400Years;
+        //    // y100 = number of whole 100-year periods within 400-year period
+        //    int y100 = n / DaysPer100Years;
+        //    // Last 100-year period has an extra day, so decrement result if 4
+        //    if (y100 == 4) y100 = 3;
+        //    // n = day number within 100-year period
+        //    n -= y100 * DaysPer100Years;
+        //    // y4 = number of whole 4-year periods within 100-year period
+        //    int y4 = n / DaysPer4Years;
+        //    // n = day number within 4-year period
+        //    n -= y4 * DaysPer4Years;
+        //    // y1 = number of whole years within 4-year period
+        //    int y1 = n / DaysPerYear;
+        //    // Last year has an extra day, so decrement result if 4
+        //    if (y1 == 4) y1 = 3;
+        //    // If year was requested, compute and return it
+        //    //if (part == DatePartYear)
+        //    //{
+        //    //    return y400 * 400 + y100 * 100 + y4 * 4 + y1 + 1;
+        //    //}
+        //    year = y400 * 400 + y100 * 100 + y4 * 4 + y1 + 1;
+        //    // n = day number within year
+        //    n -= y1 * DaysPerYear;
+        //    // If day-of-year was requested, return it
+        //    //if (part == DatePartDayOfYear) return n + 1;
+        //    // Leap year calculation looks different from IsLeapYear since y1, y4,
+        //    // and y100 are relative to year 1, not year 0
+        //    bool leapYear = y1 == 3 && (y4 != 24 || y100 == 3);
+        //    int[] days = leapYear ? DaysToMonth366 : DaysToMonth365;
+        //    // All months have less than 32 days, so n >> 5 is a good conservative
+        //    // estimate for the month
+        //    int m = (n >> 5) + 1;
+        //    // m = 1-based month number
+        //    while (n >= days[m]) m++;
+        //    // If month was requested, return it
+        //    //if (part == DatePartMonth) return m;
+        //    month = m;
+        //    // Return 1-based day-of-month
+        //    //return n - days[m - 1] + 1;
+        //    day = n - days[m - 1] + 1;
+        //}
+
+        ////--------------------------------------------------------------------------------
+
+        //public struct DateTimeContext
+        //{
+        //    public int Year;
+        //    public int Month;
+        //    public int Day;
+        //    public long Ticks;
+        //}
+
+        //public unsafe interface IDateTimeBlockConverter
+        //{
+        //    // bool CanRead { get; }
+        //    //int Read(byte* pBytes);
+
+        //    void Write(byte* pBytes, ref DateTimeContext context);
+        //}
+
+        ////--------------------------------------------------------------------------------
+
+        //public sealed class Year4BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Year4BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed(context.Year, out var div, out var mod);
+        //        *(ptr + 3) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *(ptr + 2) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        *pBytes = (byte)(Num0 + div);
+        //    }
+        //}
+
+        //public sealed class Year2BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Year2BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed(context.Year, out var div, out var mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *ptr = (byte)(Num0 + mod);
+        //    }
+        //}
+
+        //public sealed class Month2BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Month2BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed(context.Month, out var div, out var mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *ptr = (byte)(Num0 + mod);
+        //    }
+        //}
+
+        //public sealed class Day2BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Day2BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed(context.Day, out var div, out var mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *ptr = (byte)(Num0 + mod);
+        //    }
+        //}
+
+        //public sealed class Hour2BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Hour2BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed((int)((context.Ticks / TicksPerHour) % 24), out var div, out var mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *ptr = (byte)(Num0 + mod);
+        //    }
+        //}
+
+        //public sealed class Minute2BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Minute2BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed((int)((context.Ticks / TicksPerMinute) % 60), out var div, out var mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *ptr = (byte)(Num0 + mod);
+        //    }
+        //}
+
+        //public sealed class Second2BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Second2BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed((int)((context.Ticks / TicksPerSecond) % 60), out var div, out var mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *ptr = (byte)(Num0 + mod);
+        //    }
+        //}
+
+        //public sealed class Millisecond3BlockConverter : IDateTimeBlockConverter
+        //{
+        //    private readonly int offset;
+
+        //    public Millisecond3BlockConverter(int offset)
+        //    {
+        //        this.offset = offset;
+        //    }
+
+        //    public unsafe void Write(byte* pBytes, ref DateTimeContext context)
+        //    {
+        //        var ptr = pBytes + offset;
+
+        //        OperationHelper.DivMod10Signed((int)((context.Ticks / TicksPerMillisecond) % 1000), out var div, out var mod);
+        //        *(ptr + 2) = (byte)(Num0 + mod);
+        //        OperationHelper.DivMod10Signed(div, out div, out mod);
+        //        *(ptr + 1) = (byte)(Num0 + mod);
+        //        *ptr = (byte)(Num0 + div);
+        //    }
+        //}
+
+        //// Number args(Func, length)
+        //// Decimal
     }
 }
