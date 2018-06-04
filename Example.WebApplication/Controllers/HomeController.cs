3484e5a1fa97d55cc552affd48d7fdaa53cfae2b
usausa <machi.pon@gmail.com>
2018-06-04 14:40:40 +0900

    Update example

--- Example.WebApplication/Controllers/HomeController.cs
+++ Example.WebApplication/Controllers/HomeController.cs
@@ -13,6 +13,7 @@
             return View();
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
