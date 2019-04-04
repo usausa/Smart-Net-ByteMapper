5cfb3f579bbf7ddb2bbf063fdeb516f5fc000c64
usausa <machi.pon@gmail.com>
2019-04-04 14:49:16 +0900

    Fix redundant code

--- Smart.IO.ByteMapper.Benchmark/GlobalSuppression.cs
+++ Smart.IO.ByteMapper.Benchmark/GlobalSuppression.cs
@@ -1,3 +1,2 @@
 ﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", Justification = "Ignore")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Ignore")]
