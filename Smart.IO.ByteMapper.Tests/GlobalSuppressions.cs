d60b890a187b400c6462a00f0fd7fe03084f04cd
usausa <machi.pon@gmail.com>
2026-05-22 15:57:11 +0900

    Update

--- Smart.IO.ByteMapper.Tests/GlobalSuppressions.cs
+++ Smart.IO.ByteMapper.Tests/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Ignore")]
